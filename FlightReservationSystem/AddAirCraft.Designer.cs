88268db77dbf48baf2cab7f9d6b4372ce13a15f5
Abdelrahman Adel <abdelrahmanadel203@gmail.com>
2023-05-21 21:59:43 +0300

    Implemented Add Aircraft For Admin Form

--- FlightReservationSystem/AddAirCraft.Designer.cs
+++ FlightReservationSystem/AddAirCraft.Designer.cs
@@ -28,215 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            CapacityTextBox = new TextBox();
-            ManufactureYearTextBox = new TextBox();
-            AirCraftYearTextBox = new TextBox();
-            ModelTextBox = new TextBox();
-            ManfucaturerTextBox = new TextBox();
-            label3 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
-            label9 = new Label();
-            AirCraftComboBox = new ComboBox();
-            AddButton = new Button();
-            AirCraftTextBox = new TextBox();
-            label2 = new Label();
-            ((System.ComponentModel.ISupportInitialize)contentSplitContainer).BeginInit();
-            contentSplitContainer.Panel1.SuspendLayout();
-            contentSplitContainer.Panel2.SuspendLayout();
-            contentSplitContainer.SuspendLayout();
-            SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CapacityTextBox = new System.Windows.Forms.TextBox();
+            this.AirCraftYearTextBox = new System.Windows.Forms.TextBox();
+            this.ModelTextBox = new System.Windows.Forms.TextBox();
+            this.ManfucaturerTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.AirCraftComboBox = new System.Windows.Forms.ComboBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.AirCraftTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.manuYearDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.contentSplitContainer)).BeginInit();
+            this.contentSplitContainer.Panel1.SuspendLayout();
+            this.contentSplitContainer.Panel2.SuspendLayout();
+            this.contentSplitContainer.SuspendLayout();
+            this.SuspendLayout();
             // 
             // contentSplitContainer
             // 
+            this.contentSplitContainer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             // 
             // contentSplitContainer.Panel2
             // 
-            contentSplitContainer.Panel2.Controls.Add(label2);
-            contentSplitContainer.Panel2.Controls.Add(AirCraftTextBox);
-            contentSplitContainer.Panel2.Controls.Add(AddButton);
-            contentSplitContainer.Panel2.Controls.Add(AirCraftComboBox);
-            contentSplitContainer.Panel2.Controls.Add(label9);
-            contentSplitContainer.Panel2.Controls.Add(label8);
-            contentSplitContainer.Panel2.Controls.Add(label7);
-            contentSplitContainer.Panel2.Controls.Add(label6);
-            contentSplitContainer.Panel2.Controls.Add(label5);
-            contentSplitContainer.Panel2.Controls.Add(label3);
-            contentSplitContainer.Panel2.Controls.Add(ManfucaturerTextBox);
-            contentSplitContainer.Panel2.Controls.Add(ModelTextBox);
-            contentSplitContainer.Panel2.Controls.Add(AirCraftYearTextBox);
-            contentSplitContainer.Panel2.Controls.Add(ManufactureYearTextBox);
-            contentSplitContainer.Panel2.Controls.Add(CapacityTextBox);
-            contentSplitContainer.Panel2.Controls.Add(label1);
-            contentSplitContainer.Size = new Size(946, 620);
-            contentSplitContainer.SplitterDistance = 184;
+            this.contentSplitContainer.Panel2.Controls.Add(this.manuYearDateTimePicker);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label2);
+            this.contentSplitContainer.Panel2.Controls.Add(this.AirCraftTextBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.AddButton);
+            this.contentSplitContainer.Panel2.Controls.Add(this.AirCraftComboBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label9);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label8);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label7);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label6);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label5);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label3);
+            this.contentSplitContainer.Panel2.Controls.Add(this.ManfucaturerTextBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.ModelTextBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.AirCraftYearTextBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.CapacityTextBox);
+            this.contentSplitContainer.Panel2.Controls.Add(this.label1);
+            this.contentSplitContainer.Size = new System.Drawing.Size(828, 465);
+            this.contentSplitContainer.SplitterDistance = 161;
             // 
             // signInButton
             // 
-            signInButton.Size = new Size(184, 57);
+            this.signInButton.Location = new System.Drawing.Point(0, 43);
+            this.signInButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signInButton.Size = new System.Drawing.Size(161, 43);
             // 
             // signUpButton
             // 
-            signUpButton.Size = new Size(184, 57);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signUpButton.Size = new System.Drawing.Size(161, 43);
             // 
             // helpButton
             // 
-            helpButton.Location = new Point(0, 536);
-            helpButton.Size = new Size(184, 84);
+            this.helpButton.Location = new System.Drawing.Point(0, 402);
+            this.helpButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.helpButton.Size = new System.Drawing.Size(161, 63);
             // 
             // exploreFlightsButton
             // 
-            exploreFlightsButton.Size = new Size(184, 76);
+            this.exploreFlightsButton.Location = new System.Drawing.Point(0, 86);
+            this.exploreFlightsButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exploreFlightsButton.Size = new System.Drawing.Size(161, 57);
             // 
             // label1
             // 
-            label1.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(262, 14);
-            label1.Name = "label1";
-            label1.Size = new Size(192, 43);
-            label1.TabIndex = 0;
-            label1.Text = "Add AirCraft";
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(229, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add AirCraft";
             // 
             // CapacityTextBox
             // 
-            CapacityTextBox.Location = new Point(526, 311);
-            CapacityTextBox.Name = "CapacityTextBox";
-            CapacityTextBox.Size = new Size(161, 27);
-            CapacityTextBox.TabIndex = 5;
-            // 
-            // ManufactureYearTextBox
-            // 
-            ManufactureYearTextBox.Location = new Point(58, 204);
-            ManufactureYearTextBox.Name = "ManufactureYearTextBox";
-            ManufactureYearTextBox.Size = new Size(165, 27);
-            ManufactureYearTextBox.TabIndex = 6;
+            this.CapacityTextBox.Location = new System.Drawing.Point(460, 207);
+            this.CapacityTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CapacityTextBox.Name = "CapacityTextBox";
+            this.CapacityTextBox.Size = new System.Drawing.Size(141, 23);
+            this.CapacityTextBox.TabIndex = 5;
             // 
             // AirCraftYearTextBox
             // 
-            AirCraftYearTextBox.Location = new Point(526, 204);
-            AirCraftYearTextBox.Name = "AirCraftYearTextBox";
-            AirCraftYearTextBox.Size = new Size(161, 27);
-            AirCraftYearTextBox.TabIndex = 7;
+            this.AirCraftYearTextBox.Location = new System.Drawing.Point(460, 136);
+            this.AirCraftYearTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AirCraftYearTextBox.Name = "AirCraftYearTextBox";
+            this.AirCraftYearTextBox.Size = new System.Drawing.Size(141, 23);
+            this.AirCraftYearTextBox.TabIndex = 7;
             // 
             // ModelTextBox
             // 
-            ModelTextBox.Location = new Point(57, 94);
-            ModelTextBox.Name = "ModelTextBox";
-            ModelTextBox.Size = new Size(164, 27);
-            ModelTextBox.TabIndex = 8;
+            this.ModelTextBox.Location = new System.Drawing.Point(50, 70);
+            this.ModelTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ModelTextBox.Name = "ModelTextBox";
+            this.ModelTextBox.Size = new System.Drawing.Size(144, 23);
+            this.ModelTextBox.TabIndex = 8;
             // 
             // ManfucaturerTextBox
             // 
-            ManfucaturerTextBox.Location = new Point(526, 94);
-            ManfucaturerTextBox.Name = "ManfucaturerTextBox";
-            ManfucaturerTextBox.Size = new Size(161, 27);
-            ManfucaturerTextBox.TabIndex = 9;
+            this.ManfucaturerTextBox.Location = new System.Drawing.Point(460, 70);
+            this.ManfucaturerTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ManfucaturerTextBox.Name = "ManfucaturerTextBox";
+            this.ManfucaturerTextBox.Size = new System.Drawing.Size(141, 23);
+            this.ManfucaturerTextBox.TabIndex = 9;
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(58, 287);
-            label3.Name = "label3";
-            label3.Size = new Size(49, 20);
-            label3.TabIndex = 11;
-            label3.Text = "Status";
-            label3.Click += label3_Click;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Status";
             // 
             // label5
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(526, 288);
-            label5.Name = "label5";
-            label5.Size = new Size(66, 20);
-            label5.TabIndex = 13;
-            label5.Text = "Capacity";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(460, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Capacity";
             // 
             // label6
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(57, 181);
-            label6.Name = "label6";
-            label6.Size = new Size(124, 20);
-            label6.TabIndex = 14;
-            label6.Text = "Manfucature Year";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Manfucature Year";
             // 
             // label7
             // 
-            label7.AutoSize = true;
-            label7.Location = new Point(57, 71);
-            label7.Name = "label7";
-            label7.Size = new Size(52, 20);
-            label7.TabIndex = 15;
-            label7.Text = "Model";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Model";
             // 
             // label8
             // 
-            label8.AutoSize = true;
-            label8.Location = new Point(526, 181);
-            label8.Name = "label8";
-            label8.Size = new Size(92, 20);
-            label8.TabIndex = 16;
-            label8.Text = "AirCraft Year";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(460, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "AirCraft Year";
             // 
             // label9
             // 
-            label9.AutoSize = true;
-            label9.Location = new Point(526, 71);
-            label9.Name = "label9";
-            label9.Size = new Size(97, 20);
-            label9.TabIndex = 17;
-            label9.Text = "Manfucaturer";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(460, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Manfucaturer";
             // 
             // AirCraftComboBox
             // 
-            AirCraftComboBox.FormattingEnabled = true;
-            AirCraftComboBox.Items.AddRange(new object[] { "Active", "Inactive", "Maintainance" });
-            AirCraftComboBox.Location = new Point(57, 310);
-            AirCraftComboBox.Name = "AirCraftComboBox";
-            AirCraftComboBox.Size = new Size(166, 28);
-            AirCraftComboBox.TabIndex = 19;
+            this.AirCraftComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.AirCraftComboBox.FormattingEnabled = true;
+            this.AirCraftComboBox.Items.AddRange(new object[] {
+            "Active",
+            "Inactive",
+            "Maintainance"});
+            this.AirCraftComboBox.Location = new System.Drawing.Point(49, 273);
+            this.AirCraftComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AirCraftComboBox.Name = "AirCraftComboBox";
+            this.AirCraftComboBox.Size = new System.Drawing.Size(146, 23);
+            this.AirCraftComboBox.TabIndex = 19;
             // 
             // AddButton
             // 
-            AddButton.Location = new Point(291, 480);
-            AddButton.Name = "AddButton";
-            AddButton.Size = new Size(146, 29);
-            AddButton.TabIndex = 20;
-            AddButton.Text = "Add";
-            AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Location = new System.Drawing.Point(255, 360);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(128, 22);
+            this.AddButton.TabIndex = 20;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // AirCraftTextBox
             // 
-            AirCraftTextBox.Location = new Point(57, 402);
-            AirCraftTextBox.Name = "AirCraftTextBox";
-            AirCraftTextBox.Size = new Size(166, 27);
-            AirCraftTextBox.TabIndex = 21;
+            this.AirCraftTextBox.Location = new System.Drawing.Point(50, 207);
+            this.AirCraftTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AirCraftTextBox.Name = "AirCraftTextBox";
+            this.AirCraftTextBox.Size = new System.Drawing.Size(146, 23);
+            this.AirCraftTextBox.TabIndex = 21;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(57, 379);
-            label2.Name = "label2";
-            label2.Size = new Size(95, 20);
-            label2.TabIndex = 22;
-            label2.Text = "AirCraft Type";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "AirCraft Type";
+            // 
+            // manuYearDateTimePicker
+            // 
+            this.manuYearDateTimePicker.CustomFormat = "yyyy";
+            this.manuYearDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.manuYearDateTimePicker.Location = new System.Drawing.Point(49, 141);
+            this.manuYearDateTimePicker.Name = "manuYearDateTimePicker";
+            this.manuYearDateTimePicker.ShowUpDown = true;
+            this.manuYearDateTimePicker.Size = new System.Drawing.Size(147, 23);
+            this.manuYearDateTimePicker.TabIndex = 23;
             // 
             // AddAirCraft
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(946, 620);
-            Name = "AddAirCraft";
-            Text = "AddAirCraft";
-            contentSplitContainer.Panel1.ResumeLayout(false);
-            contentSplitContainer.Panel2.ResumeLayout(false);
-            contentSplitContainer.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)contentSplitContainer).EndInit();
-            contentSplitContainer.ResumeLayout(false);
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 465);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "AddAirCraft";
+            this.Text = "AddAirCraft";
+            this.contentSplitContainer.Panel1.ResumeLayout(false);
+            this.contentSplitContainer.Panel2.ResumeLayout(false);
+            this.contentSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.contentSplitContainer)).EndInit();
+            this.contentSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -251,11 +274,11 @@
         private TextBox ManfucaturerTextBox;
         private TextBox ModelTextBox;
         private TextBox AirCraftYearTextBox;
-        private TextBox ManufactureYearTextBox;
         private TextBox CapacityTextBox;
         private Button AddButton;
         private ComboBox AirCraftComboBox;
         private Label label2;
         private TextBox AirCraftTextBox;
+        private DateTimePicker manuYearDateTimePicker;
     }
 }
--- FlightReservationSystem/AddAirCraft.cs
+++ FlightReservationSystem/AddAirCraft.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace FlightReservationSystem
 {
@@ -17,13 +20,26 @@ namespace FlightReservationSystem
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void AddButton_Click(object sender, EventArgs e)
         {
+            //TODO: Check Regex Amd Validate Input
+            using (SqlConnection connection = new SqlConnection(databaseConnection))
+            {
+                connection.Open();
+                string query = "INSERT INTO Aircraft VALUES(@Model,@Manufacturer,@AircraftType,@ManufactureYear,@Capacity,@Status)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Model",ModelTextBox.Text);
+                command.Parameters.AddWithValue("@Manufacturer", ManfucaturerTextBox.Text);
+                command.Parameters.AddWithValue("@AircraftType", AirCraftTextBox.Text);
+                command.Parameters.AddWithValue("@ManufactureYear", DateTime.ParseExact(manuYearDateTimePicker.Text, "yyyy", CultureInfo.CurrentCulture).ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("@Capacity", int.Parse(CapacityTextBox.Text));
+                command.Parameters.AddWithValue("@Status", AirCraftComboBox.Text);
+                //string expectedArrivalDate = DateTime.ParseExact(ArrivaldateTimePicker.Text, "dddd, MMMM d, yyyy", CultureInfo.CurrentCulture).ToString("yyyy-MM-dd");
+                //SqlDataAdapter adapter = new SqlDataAdapter(command);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
         }
     }
 }
